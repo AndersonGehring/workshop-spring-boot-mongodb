0cca5ea20df8452bfae8b9e988734287e0365874
Anderson Gehring <andersondrive15@gmail.com>
2023-10-31 20:26:43 -0300

    Referenciando os posts do usuário

--- src/main/java/com/andersongehring/workshopmongo/domain/User.java
+++ src/main/java/com/andersongehring/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.andersongehring.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -14,6 +17,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) //(lazy = true) = quando carregar o user não vai vir a list junto, apenas quando referenciar o Post
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {		
 	}
@@ -47,6 +53,14 @@ public class User implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}	
 
 	@Override
 	public int hashCode() {
@@ -63,5 +77,5 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
-	}		
+	}
 }
